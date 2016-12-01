47fcd2cfc128dd3998b835dbdf212f51b279dd0d
Adam <ad@mharrison.us>
2016-12-01 18:31:07 -0500

    Expand TypeSubject

--- src/main/java/com/ulfric/verify/TypeSubject.java
+++ src/main/java/com/ulfric/verify/TypeSubject.java
@@ -17,35 +17,70 @@ public class TypeSubject<T extends Type> extends Subject<T> {
 	{
 		if (this.subject instanceof Class) return;
 
-		this.fail(" any Class");
+		this.fail("any Class");
+	}
+
+	public final void isNotClass()
+	{
+		if (!(this.subject instanceof Class)) return;
+
+		this.fail("anything other than a Class");
 	}
 
 	public final void isParameterizedType()
 	{
 		if (this.subject instanceof ParameterizedType) return;
 
-		this.fail(" a ParameterizedType");
+		this.fail("a ParameterizedType");
+	}
+
+	public final void isNotParameterizedType()
+	{
+		if (!(this.subject instanceof ParameterizedType)) return;
+
+		this.fail("anything other than a ParameterizedType");
 	}
 
 	public final void isGenericArrayType()
 	{
 		if (this.subject instanceof GenericArrayType) return;
 
-		this.fail(" a GenericArrayType");
+		this.fail("a GenericArrayType");
+	}
+
+	public final void isNotGenericArrayType()
+	{
+		if (!(this.subject instanceof GenericArrayType)) return;
+
+		this.fail("anything other than a GenericArrayType");
 	}
 
 	public final void isWildcardType()
 	{
 		if (this.subject instanceof WildcardType) return;
 
-		this.fail(" a WildcardType");
+		this.fail("a WildcardType");
+	}
+
+	public final void isNotWildcardType()
+	{
+		if (!(this.subject instanceof WildcardType)) return;
+
+		this.fail("anything other than a WildcardType");
 	}
 
 	public final void isTypeVariable()
 	{
 		if (this.subject instanceof TypeVariable) return;
 
-		this.fail(" a TypeVariable");
+		this.fail("a TypeVariable");
+	}
+
+	public final void isNotTypeVariable()
+	{
+		if (!(this.subject instanceof TypeVariable)) return;
+
+		this.fail("anything other than a TypeVariable");
 	}
 
 }
