0228da852822db9013c8d9b01c5779d13cf21dbe
insou22 <zac.kologlu@gmail.com>
2017-01-07 21:39:00 +1100

    oops

--- src/main/java/com/ulfric/verify/SupplierSubject.java
+++ src/main/java/com/ulfric/verify/SupplierSubject.java
@@ -1,5 +1,7 @@
 package com.ulfric.verify;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+
 import com.ulfric.verify.function.CheckedSupplier;
 import com.ulfric.verify.verb.SupplierVerb;
 
@@ -13,16 +15,9 @@ class SupplierSubject extends RunnableSubject<CheckedSupplier<?>> implements Sup
 	@Override
 	public final void suppliesNonUniqueValues()
 	{
-		try
+		if (this.getValue() == this.getValue())
 		{
-			if (this.subject.get() == this.subject.get())
-			{
-				return;
-			}
-		}
-		catch (Throwable e)
-		{
-			throw new RuntimeException(e);
+			return;
 		}
 
 		this.error("expected non-unique values to be supplied, was unique");
@@ -31,19 +26,154 @@ class SupplierSubject extends RunnableSubject<CheckedSupplier<?>> implements Sup
 	@Override
 	public final void suppliesUniqueValues()
 	{
-		try
+		if (this.getValue() != this.getValue())
 		{
-			if (this.subject.get() != this.subject.get())
-			{
-				return;
-			}
-		}
-		catch (Throwable e)
-		{
-			throw new RuntimeException(e);
+			return;
 		}
 
 		this.error("expected unique values to be supplied, was non-unique");
+	}
+
+	@Override
+	public final void valueIsNotNull()
+	{
+		Object subject = this.getValue();
+
+		if (subject != null)
+		{
+			return;
+		}
+
+		this.error("expected not null, was " + subject);
+	}
+
+	@Override
+	public final void valueIsNull()
+	{
+		Object subject = this.getValue();
+
+		if (subject == null)
+		{
+			return;
+		}
+
+		this.error("expected null, was not null");
+	}
+
+	@Override
+	public final void valueIsSameAs(Object other)
+	{
+		Object subject = this.getValue();
+
+		if (subject == other)
+		{
+			return;
+		}
+
+		this.error("expected the same as " + other + ", was " + subject);
+	}
+
+	@Override
+	public final void valueIsNotSameAs(Object other)
+	{
+		Object subject = this.getValue();
+
+		if (subject != other)
+		{
+			return;
+		}
+
+		this.error("expected anything except for " + ", was the same");
+	}
+
+	@Override
+	public final void valueIsEqualTo(Object other)
+	{
+		Object subject = this.getValue();
+
+		if (EqualsBuilder.reflectionEquals(subject, other))
+		{
+			return;
+		}
+
+		this.error("expected equal to " + other + ", was " + subject);
+	}
+
+	@Override
+	public final void valueIsNotEqualTo(Object other)
+	{
+		Object subject = this.getValue();
+
+		if (!EqualsBuilder.reflectionEquals(subject, other))
+		{
+			return;
+		}
+
+		this.error("expected not equal to " + other + ", was " + subject);
+	}
+
+	@Override
+	public final void valueIsInstanceOf(Class<?> clazz)
+	{
+		Object subject = this.getValue();
+
+		if (clazz.isInstance(subject))
+		{
+			return;
+		}
+
+		this.error("expected an instance of " + clazz + ", was " + subject.getClass());
+	}
+
+	@Override
+	public final void valueIsNotInstanceOf(Class<?> clazz)
+	{
+		Object subject = this.getValue();
+
+		if (!clazz.isInstance(subject))
+		{
+			return;
+		}
+
+		this.error("expected not an instance of " + clazz + ", was an instance of the same type");
+	}
+
+	@Override
+	public final void valueIsExactType(Class<?> clazz)
+	{
+		Object subject = this.getValue();
+
+		if (clazz == subject.getClass())
+		{
+			return;
+		}
+
+		this.error("expected an instance of exactly " + clazz + ", was an instance of " + subject.getClass());
+	}
+
+	@Override
+	public final void valueIsNotExactType(Class<?> clazz)
+	{
+		Object subject = this.getValue();
+
+		if (clazz != subject.getClass())
+		{
+			return;
+		}
+
+		this.error("expected not an instance of exactly " + clazz + ", was an instance of the same type");
+	}
+
+	private Object getValue()
+	{
+		try
+		{
+			return this.subject.get();
+		}
+		catch (Throwable throwable)
+		{
+			throw new RuntimeException(throwable);
+		}
 	}
 
 }
--- src/main/java/com/ulfric/verify/verb/SupplierVerb.java
+++ src/main/java/com/ulfric/verify/verb/SupplierVerb.java
@@ -6,4 +6,25 @@ public interface SupplierVerb extends RunnableVerb {
 
 	void suppliesUniqueValues();
 
+	void valueIsNotNull();
+
+	void valueIsNull();
+
+	void valueIsSameAs(Object other);
+
+	void valueIsNotSameAs(Object other);
+
+	void valueIsEqualTo(Object other);
+
+	void valueIsNotEqualTo(Object other);
+
+	void valueIsInstanceOf(Class<?> clazz);
+
+	void valueIsNotInstanceOf(Class<?> clazz);
+
+	void valueIsExactType(Class<?> clazz);
+
+	void valueIsNotExactType(Class<?> clazz);
+
+
 }
